a955dbbab7ae8fa9f5deb647a45b7aa5e99b238f
Ellen Silva <virginiaellen3@gmail.com>
2024-01-09 13:40:42 -0300

    Resposta do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"112222", modelo: "Modelo 8", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Genshin Impact");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"256398", modelo: "Modelo 7", imei: "222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Stardew Valley");
